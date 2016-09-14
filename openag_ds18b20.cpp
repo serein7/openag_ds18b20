6c61a9d4731ef20f6703674e594115c6092188c0
serein7 <seremin@gmail.com>
2016-09-14 19:23:18 +0300

    Update openag_ds18b20.cpp

--- openag_ds18b20.cpp
+++ openag_ds18b20.cpp
@@ -23,7 +23,7 @@ void Ds18b20::begin() {
 
 void Ds18b20::update() {
   if (_waiting_for_conversion) {
-    if (millis() - _time_of_last_query > 500 && _sensors.isConversionComplete()) {
+    if (millis() - _time_of_last_query > 600 && _sensors.isConversionComplete()) {
       status_level = OK;
       status_msg = "";
       _waiting_for_conversion = false;
